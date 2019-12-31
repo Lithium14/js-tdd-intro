b8b012c244cf5ec3fede956ce9e24a242fc0f2de
Lithium14 <fadi.rata@live.fr>
2019-12-31 16:25:35 +0100

    feat: git tag step-2.

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,6 +1,3 @@
-
-const assert = require('assert');
-
 function capitalizeFirstLetters(input) {
     return input.split(' ').map((letter) => {
         return letter.charAt(0).toUpperCase() + letter.slice(1);
@@ -9,12 +6,4 @@ function capitalizeFirstLetters(input) {
 
 
 
-assert.strictEqual(typeof capitalizeFirstLetters, 'function');
-
-assert.strictEqual(capitalizeFirstLetters.length, 1);
-
-assert.strictEqual(capitalizeFirst('i am learning TDD'), 'I Am Learning TDD');
-
-assert.strictEqual(capitalizeFirst('z'), 'Z');
-
-assert.strictEqual(capitalizeFirst(''), '');
+module.exports = capitalizeFirstLetters;
